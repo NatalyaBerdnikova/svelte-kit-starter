da6c1a874e17b03b5f3c2f6f0d4a1941fd31f4e2
Natalya Berdnikova <kitakava@mail.ru>
2023-04-16 21:37:04 +0500

    Add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		outDir: 'dist',
-		adapter: adapter()
-	}
+  kit: {
+    outDir: "dist",
+    adapter: adapter(),
+  },
 };
 
 export default config;
